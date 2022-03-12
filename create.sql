028b59dbea86e772c395404aeb4a4e75bce1052e
Pedro Aibar <75799784+motinsa@users.noreply.github.com>
2022-03-12 16:59:00 +0100

    Update create.sql

---
+++ create.sql
@@ -0,0 +1,52 @@
+#CREATE DATABASE ajedrez;
+
+CREATE TABLE USUARIO
+(
+   Nickname     VARCHAR(30) PRIMARY KEY,
+   contraseña   VARCHAR(30)        NOT NULL,
+   puntos       INT        NOT NULL,
+   monedas      INT       NOT NULL,
+   avatar       VARCHAR(30),
+   piezas       VARCHAR(30),
+   tablero      VARCHAR(30)        
+);
+
+CREATE TABLE amigos
+(
+   valor              VARCHAR(30),
+   USUARIO_Nickname   VARCHAR(30),
+   PRIMARY KEY (valor,USUARIO_Nickname),
+   FOREIGN KEY (USUARIO_Nickname) REFERENCES USUARIO(Nickname)
+);
+
+CREATE TABLE PARTIDA
+(
+   id        INT PRIMARY KEY,
+   ganador   BOOLEAN        NOT NULL
+);
+
+CREATE TABLE ARTICULO
+(
+   nombre   VARCHAR(30) PRIMARY KEY,
+   precio   INT        NOT NULL,
+   tipo     VARCHAR(30)        NOT NULL
+);
+
+CREATE TABLE juega
+(
+   USUARIO_Nickname   VARCHAR(30),
+   PARTIDA_id         INT,
+   PRIMARY KEY (USUARIO_Nickname,PARTIDA_id),
+   FOREIGN KEY (USUARIO_Nickname) REFERENCES USUARIO(Nickname),
+   FOREIGN KEY (PARTIDA_id) REFERENCES PARTIDA(id)
+);
+
+CREATE TABLE compra
+(
+   USUARIO_Nickname   VARCHAR(30),
+   ARTICULO_nombre   VARCHAR(30),
+   PRIMARY KEY (USUARIO_Nickname,ARTICULO_nombre),
+   FOREIGN KEY (USUARIO_Nickname) REFERENCES USUARIO(Nickname),
+   FOREIGN KEY (ARTICULO_nombre) REFERENCES ARTICULO(nombre)
+);
+
