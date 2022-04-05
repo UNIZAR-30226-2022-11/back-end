7469ae4f5e096a7c2188e68f9e3331e2f96417da
JorgeMG117 <jorgemartinezgil117@gmail.com>
2022-04-05 22:57:04 +0200

    Cambio

--- models/user.js
+++ models/user.js
@@ -1,15 +1,38 @@
 var con = require('../configs/database')
 
 // constructor
-const User = function(user) {
-  this.nickname = user.nickname;
-  this.password = user.password;
-  this.puntos = user.puntos;
-  this.monedas = user.monedas;
-  this.avatar = user.avatar;
-  this.piezas = user.piezas;
-  this.tablero = user.tablero;
-};
+class User {
+  constructor(user) {
+    this.nickname = user.nickname;
+    this.password = user.password;
+    this.puntos = user.puntos;
+    this.monedas = user.monedas;
+    this.avatar = user.avatar;
+    this.piezas = user.piezas;
+    this.tablero = user.tablero;
+  }
+  static create(_nickname, _password, result) {
+    var sql = "insert into usuario(Nickname,contraseña,puntos,monedas,avatar,piezas,tablero) values (\"" +
+      _nickname + "\", \"" +
+      _password + "\", " +
+      "0, 0, 0, 0, 0);";
+    con.query(sql, function (err, result) {
+      if (err)
+        throw err;
+    });
+  }
+  static async getUserByNickname(_nickname, result) {
+    var sql = "SELECT * FROM usuario WHERE Nickname = \"" + _nickname + "\"";
+    let rows = await query(sql);
+
+    const user = new User({
+      nickname: rows[0].Nickname,
+      password: rows[0].contraseña
+    });
+
+    return user;
+  }
+}
 
 let query = function( sql, values ) {
   // devolver una promesa
@@ -26,26 +49,6 @@ return new Promise(( resolve, reject ) => {
 })
 }
 
-User.create = (_nickname, _password, result) => {
-  var sql = "insert into usuario(Nickname,contraseña,puntos,monedas,avatar,piezas,tablero) values (\"" + 
-    _nickname + "\", \"" +
-    _password + "\", " +
-    "0, 0, 0, 0, 0);";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-  });
-};
 
-User.getUserByNickname = async (_nickname, result) => {
-  var sql ="SELECT * FROM usuario WHERE Nickname = \"" + _nickname + "\""
-  let rows = await query(sql)
-  
-  const user = new User({
-    nickname: rows[0].Nickname,
-    password : rows[0].contraseña
-  })
-
-  return user
-};
   
 module.exports = User;
