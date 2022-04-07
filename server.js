489b607ef2edac90146078aecc00b20868ab2c56
motinsa <pedroaibarmarco@gmail.com>
2022-04-07 12:53:00 +0200

    prueba

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const indexRouter = require('./routes/index')
 const loginRouter = require('./routes/login')
 const registerRouter = require('./routes/register')
 
-app.use('/', indexRouter)
+app.use(indexRouter)
 app.use('/login', loginRouter)
 app.use('/register', registerRouter)
 
