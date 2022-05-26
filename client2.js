f3a5668766cd37e5b7d3739a9d2c7dac9a0fda5d
JorgeMG117 <jorgemartinezgil117@gmail.com>
2022-05-26 16:32:17 +0200

    Usuarios conectados

--- client2.js
+++ client2.js
@@ -3,6 +3,14 @@ const socket = io("http://localhost:3001")
 
 opponent = "";
 socket.on('connect',() =>{
-    console.log(socket.connected);
+    socket.emit('conectarse', process.argv.slice(2)[0])
 })
 
+socket.on('usuariosConectados', message => {
+    console.log("usuariosConectados")
+    console.log(message)
+})
+
+if(process.argv.slice(3)[0] != 0){
+    socket.emit('ingame', process.argv.slice(2)[0])
+}
--- configs/socket_peticiones.js
+++ configs/socket_peticiones.js
@@ -5,13 +5,36 @@ const io = require('socket.io')(3001, {
 })
 
 let sockets = new Map()
+let nicknames = new Map()
+let partida = new Map()
 
 io.on('connection', socket => {  
     console.log('new connection peticiones');
     console.log(socket.id)
+    let intervalId
 
     socket.on('conectarse', (nickname) =>{
         sockets.set(nickname, socket.id)
+        nicknames.set(socket.id, nickname)
+
+        intervalId = setInterval(function() {
+            let concectados = []
+            for (let key of sockets.keys()) {
+                if(partida.get(key)){
+                    concectados.push({ nickname: key, partida: 1 })
+                }
+                else{
+                    concectados.push({ nickname: key, partida: 0 })
+                }
+            }
+
+            socket.emit('usuariosConectados', { usuarios: concectados })
+        }, 3000);
+    })
+
+    socket.on('ingame', (nickname) =>{
+        console.log("ingame")
+        partida.set(nickname, 1)
     })
 
     socket.on('inviteFriend', (nickname, friend) =>{
@@ -24,6 +47,17 @@ io.on('connection', socket => {
         console.log("inviteFriend")
 
         socket.to(sockets.get(friend)).emit('getFriendOpponent', { nick: nickname })
+    })
+
+    socket.on("disconnecting", () => {
+        console.log("disconnecting")
+        clearInterval(intervalId)
+        
+        //Eliminar info de los maps
+        console.log(nicknames.get(socket.id))
+        sockets.delete(nicknames.get(socket.id))
+        partida.delete(nicknames.get(socket.id))
+        nicknames.delete(socket.id)
     })
 })
 
